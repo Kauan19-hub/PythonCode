1eaba4fc7aefd55257e1fe8f3ccab2626115127e
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-08-27 22:26:33 -0300

    Update Desafio3 - Hab.py

--- Desafio3 - Hab.py
+++ Desafio3 - Hab.py
@@ -1,27 +1,29 @@
 habitantes = []
 
 def coleta_dados():
-  
   for i in range(4):
     print(f"\nHabitante: ")
     
     nome = input("Nome: ")
     idade = int(input("Idade: "))
     altura = float(input("Altura: "))
+    genero = input("Genero M/F: ")
     esporte = input(("Esporte favorito: "))
     
     habitantes.append ({
       'nome': nome,
       'idade': idade,
       'altura': altura,
+      'genero': genero,
       'esporte': esporte,
+      
     })
     
 def sem_homem_natacao():
   print("Infelizmente, não há homens que gostam de natação...")
   
 def media_idade_homens_natacao(pessoas):
-  homens = [p['idade'] for p in pessoas if p[esporte] == ['natação']]
+  homens = [p['idade'] for p in pessoas if p['genero'] == 'M' and p['esporte'] == 'natação']
   
   if homens:
     media = sum(homens) / len(homens)
@@ -30,7 +32,7 @@ def media_idade_homens_natacao(pessoas):
   else:
     sem_homem_natacao()
     
-sem_homem_natacao()
-    
+coleta_dados()
+media_idade_homens_natacao(habitantes)    
     
     
